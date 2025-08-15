be16f05b25c1b06480960a9a0b594ac3c4637f58
Firoooooo <firat.yildiz@gmx.de>
2025-08-15 11:06:40 +0200

    Init Check In

---
+++ Units/UnitTExamples/AXNamespace.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTExamples
+{
+    public class AXNamespace
+    {
+        public static string GetNamespaceForType(AXType _xMLType)
+        {
+            string ret = string.Empty;
+
+            switch (_xMLType)
+            {
+                case AXType.AxMenuItemDisplay:
+                case AXType.AxMenuItemAction:
+                case AXType.AxMenuItemOutput:
+                case AXType.AxMenu:
+                case AXType.AxMenuExtension:
+                    ret = "Microsoft.Dynamics.AX.Metadata.V1";
+                    break;
+
+                case AXType.AxForm:
+                    ret = "Microsoft.Dynamics.AX.Metadata.V6";
+                    break;
+
+                case AXType.AxReport:
+                    ret = "Microsoft.Dynamics.AX.Metadata.V2";
+                    break;
+
+                default:
+                    ret = String.Empty;
+                    break;
+            }
+
+            return ret;
+        }
+    }
+}
